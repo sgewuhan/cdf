9b5019e41cff1c647adc24ea2416df0bb2fd3264
ghuazh <ghuazh@gmail.com>
2014-08-14 16:03:49 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ com.sg.cdf.osgiclientdemo/src/com/sg/cdf/osgiclientdemo/Activator.java
@@ -0,0 +1,98 @@
+package com.sg.cdf.osgiclientdemo;
+
+import java.util.ArrayList;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.sg.cdf.ws.service.DistributionService;
+import com.sg.cdf.ws.service.NameSpace;
+import com.sg.cdf.ws.service.Parameter;
+import com.sg.cdf.ws.service.RequestBuilder;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	@SuppressWarnings("rawtypes")
+	private ServiceTracker tracker;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		tracker = new ServiceTracker(bundleContext, DistributionService.class.getName(), null) {
+            @Override
+            public Object addingService(ServiceReference reference) {
+                Object service = super.addingService(reference);
+                if (service instanceof DistributionService) {
+                    useService((DistributionService) service);
+                }
+                return service;
+            }
+        };
+        tracker.open();
+	}
+
+	protected void useService(DistributionService service) {
+		RequestBuilder factory  = new RequestBuilderImpl();
+		factory.setName("Web Service 调用内容服务并传递参数");
+		
+		NameSpace nameSpace = new NameSpace();
+		nameSpace.setBundle("com.sg.cdf.persistence.mongodb");
+		nameSpace.setClas("com.sg.cdf.persistence.mongodb.MongoDBJsonDistribution");
+		factory.setPersistence(nameSpace);
+
+		//传递参数
+		ArrayList<Parameter> parameterList = new ArrayList<Parameter>();
+		
+		Parameter parameter = new Parameter();
+		parameter.setName("templateURL");
+		parameter.setValue("http://localhost:10080/html/sample.rptdesign");
+		parameterList.add(parameter);
+		
+		parameter = new Parameter();
+		parameter.setName("dataURL");
+		parameter.setValue("http://localhost:10080/html/xmlpo.xml");
+		parameterList.add(parameter);
+
+		factory.setParameters(parameterList);
+		
+		//注入内容提供者
+		nameSpace = new NameSpace();
+		nameSpace.setBundle("com.sg.cdf.customerized");
+		nameSpace.setClas("com.sg.cdf.demo.demo2.contentprovider.ContentProviderDemo");
+		factory.setContentProvider(nameSpace);
+		
+		//注入发布器
+		ArrayList<NameSpace> nameSpaceList = new ArrayList<NameSpace>();
+		nameSpace = new NameSpace();
+		nameSpace.setBundle("com.sg.cdf.customerized");
+		nameSpace.setClas("com.sg.cdf.demo.demo2.distributor.DistributorByEmailWithParameter");
+		nameSpaceList.add(nameSpace);
+		
+		factory.setDistributors(nameSpaceList);
+		
+		service.distribute(factory);
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		tracker.close();
+		Activator.context = null;
+	}
+
+}
